312c43338a84773e57ad06b0ed60f7c35f7fc1e6
L-oris <loris4526@gmail.com>
2020-11-08 19:23:06 +0100

    program outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/* OUTLINE:
+ *
+ * Read <filename.md> (first arg) to String
+ * Remove empty lines and those beginning with `#` and `---`
+ * Sort lines by customized sorting fn
+ * * if line starts with `to `, pick second word
+ * * sort alphabetically
+ * Group lines by letter
+ * Begin each letter-group with `### <letter>`
+ * Push `### Mobile` and `### PC` to beginning of String
+ * Write to `sorted.md`
+ */
