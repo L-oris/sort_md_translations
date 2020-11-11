1169f345080391b65299b44cee0984635629eea6
L-oris <loris4526@gmail.com>
2020-11-11 21:29:18 +0100

    parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
+use std::env;
 use std::error::Error;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let raw_content = fs::read_to_string("sample.md")?;
+    let args = env::args().collect::<Vec<String>>();
+    let (source, destination) = parse_config(&args);
+    let raw_content = fs::read_to_string(source)?;
 
     let mut grouped_by_first_letter: HashMap<char, Vec<String>> = raw_content
         .lines()
@@ -20,8 +23,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             acc
         });
 
-    // println!("{:#?}", grouped_by_first_letter);
-
     let mut output = format!("### Mobile\n\n\n### Laptop\n\n\n---\n\n");
     for first_letter in 'a'..='z' {
         let lines = grouped_by_first_letter.get_mut(&first_letter);
@@ -36,9 +37,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("{}", output);
-
+    fs::write(destination, output.as_bytes())?;
     Ok(())
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    if args.len() < 3 {
+        panic!("Not enough arguments provided: `sort_md_translation input.md output.md`");
+    }
+
+    let source = &args[1];
+    let destination = &args[2];
+    (source, destination)
 }
 
 trait ParticleRemover {
